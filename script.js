91c7c21006f2b6a799f195b8ec7ecfe9da54cf54
John Potts <john@arlingtonchurch.com>
2017-08-16 09:17:07 -0400

    Updated CSS, fixed timer

--- script.js
+++ script.js
@@ -1,40 +1,104 @@
+var secondsLabel = document.getElementById('seconds'),
+  minutesLabel = document.getElementById('minutes'),
+  breakTimeLabel =document.getElementById('breakTime'),
+  workTimeLabel =document.getElementById('workTime'),
+  totalSeconds = 0,
+  startWorkButton = document.getElementById('startWork'),
+  startBreakButton = document.getElementById('startBreak'),
+  pauseButton = document.getElementById('pause'),
+  resetButton = document.getElementById('reset'),
+  addWorkButton = document.getElementById('addWork'),
+  addBreakButton = document.getElementById('addBreak'),
+  subtractWorkButton = document.getElementById('subtractWork'),
+  subtractBreakButton = document.getElementById('subtractBreak'),
+  timer = null,
+  workTime=25,
+  breakTime=5;
 
-  var secondsLabel = document.getElementById('seconds'), minutesLabel= document.getElementById('minutes'),
-hoursLabel= document.getElementById('hours'), totalSeconds=0, startButton = document.getElementById('start'),
-pauseButton= document.getElementById('pause'),resetButton= document.getElementById('reset'),timer=null;
-
-startButton.onclick=function(){
-  if(!timer) {
-    timer=setInterval(setTime,1000);
-  }
-};
-
-pauseButton.onclick= function (){
-  if(timer) {
-    clearInterval(timer);
-    timer=null;
-  }
-};
-
-resetButton.onclick=function() {
-    totalSeconds=0;
-    clearInterval(timer);
-    timer=null;
-    secondsLabel.innerHTML="00";
-    minutesLabel.innerHTML="25:";
-
-  };
-function setTime(){
-  totalSeconds++;
-  secondsLabel.innerHTML=pad(60-(totalSeconds % 60));
-  minutesLabel.innerHTML=pad(parseInt(25-(totalSeconds/60)))+":";
-  hoursLabel.innerHTML=pad(parseInt(totalSeconds/3600))+":";
+addWorkButton.onclick = function() {
+  workTime+=1; workTimeLabel.innerHTML = workTime;
 }
-function pad(val) {
-  var valString = val + "";
-  if (valString.length<2) {
-    return "0"+valString;
+addBreakButton.onclick = function() {
+    breakTime+=1;breakTimeLabel.innerHTML = breakTime;
+}
+subtractWorkButton.onclick = function() {
+    workTime-=1;workTimeLabel.innerHTML = workTime;
+}
+subtractBreakButton.onclick = function() {
+    breakTime-=1;breakTimeLabel.innerHTML = breakTime;
+}
+
+
+startWorkButton.onclick = function() {
+  if (!timer) {
+    timer = setInterval(setWorkTime, 1000);
   }
-  else{
+};
+
+startBreakButton.onclick = function() {
+  if (!timer) {
+    timer = setInterval(setBreakTime, 1000);
+  }
+};
+
+pauseButton.onclick = function() {
+  if (timer) {
+    clearInterval(timer);
+    timer = null;
+  }
+};
+
+resetButton.onclick=function(){
+  totalSeconds = 0;
+  clearInterval(timer);
+  timer = null;
+  secondsLabel.innerHTML = "00";
+  minutesLabel.innerHTML = "00:";
+};
+
+function setWorkTime() {
+var timerOne;
+  totalSeconds++;
+  if (totalSeconds % 60 === 0){timerOne = pad(0)}
+  else{timerOne= pad(60 - (totalSeconds % 60));};
+  var timerTwo= pad(parseInt(workTime - (totalSeconds / 60)));
+
+  secondsLabel.innerHTML = timerOne;
+  minutesLabel.innerHTML = timerTwo + ":";
+  if (timerOne=="00" && timerTwo == "00"){
+    totalSeconds = 0;
+    clearInterval(timer);
+    timer = null;
+    secondsLabel.innerHTML = "00";
+    minutesLabel.innerHTML = "00:";
+    //play audio
+  }
+
+}
+
+function setBreakTime() {
+  var timerOne;
+    totalSeconds++;
+    if (totalSeconds % 60 === 0){timerOne = pad(0)}
+    else{timerOne= pad(60 - (totalSeconds % 60));};
+    var timerTwo= pad(parseInt(breakTime - (totalSeconds / 60)));
+
+    secondsLabel.innerHTML = timerOne;
+    minutesLabel.innerHTML = timerTwo + ":";
+    if (timerOne=="00" && timerTwo == "00"){
+      totalSeconds = 0;
+      clearInterval(timer);
+      timer = null;
+      secondsLabel.innerHTML = "00";
+      minutesLabel.innerHTML = "00:";
+      //play audio
+    }
+  }
+function pad(val) {
+  var valString = String(val);
+  if (valString.length < 2) {
+    return "0" + valString;
+  } else {
     return valString;
-}};
+  }
+};
